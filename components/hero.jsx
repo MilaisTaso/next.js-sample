a8ca83c63bcc30fddbc9df7121b39307c243f385
MilaisTaso <livreblanche819@gmail.com>
2023-03-28 10:22:24 +0900

    add next/image

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,4 +1,6 @@
 import styles from "styles/hero.module.css";
+import cube from "images/cube.jpg";
+import Image from "next/image";
 export default function Hero({ title, subtitle, imageOn = false }) {
   return (
     <div className={styles.flexContainer}>
@@ -6,7 +8,20 @@ export default function Hero({ title, subtitle, imageOn = false }) {
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
       </div>
-      {imageOn && <figure>[画像]</figure>}
+      {imageOn && (
+        <figure>
+          <Image
+            src={cube}
+            width={576}
+            height={499}
+            alt=""
+            sizes="(min-width: 1152px) 576px, (min-width: 768) 50vw, 100vw"
+            style={{ width: "100%", height: "auto" }}
+            priority
+            placeholder="blur"
+          />
+        </figure>
+      )}
     </div>
   );
 }
