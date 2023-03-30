c93d7c2e20f089d1044e0cb58495d998d2ad27b0
MilaisTaso <livreblanche819@gmail.com>
2023-03-31 08:32:44 +0900

    add post-header components

--- pages/blog/schedule.jsx
+++ pages/blog/schedule.jsx
@@ -1,21 +1,34 @@
-import { client } from "lib/api";
+import Container from "@/components/container";
+import PostHeader from "@/components/post-header";
+import { getPostBySlug } from "lib/api";
 
-export default function Schedule() {
-  return <h1>記事のタイトル</h1>;
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+      <article>
+        <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+      </article>
+    </Container>
+  );
 }
 
 export async function getStaticProps() {
-  const resPromise = await client.get({
-    endpoint: "blogs",
-  });
+  const slug = "schedule";
+  const post = await getPostBySlug(slug);
 
-  try {
-    const res = await resPromise;
-    console.log(res);
-  } catch (err) {
-    console.log(err);
-  }
   return {
-    props: {},
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
   };
 }
