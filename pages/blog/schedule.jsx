fc8719eedd2da62190ad98d2e8348039d6b46d30
MilaisTaso <livreblanche819@gmail.com>
2023-03-30 14:33:13 +0900

    edit api components

--- pages/blog/schedule.jsx
+++ pages/blog/schedule.jsx
@@ -1,12 +1,14 @@
 import { client } from "lib/api";
+
 export default function Schedule() {
   return <h1>記事のタイトル</h1>;
 }
 
 export async function getStaticProps() {
-  const resPromise = client.get({
+  const resPromise = await client.get({
     endpoint: "blogs",
   });
+
   try {
     const res = await resPromise;
     console.log(res);
