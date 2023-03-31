abc6a1c5f13023ff02e7e20d9655031e7d140493
MilaisTaso <livreblanche819@gmail.com>
2023-03-31 16:36:41 +0900

    add eyecatch blogs/schedule

--- pages/blog/schedule.jsx
+++ pages/blog/schedule.jsx
@@ -1,5 +1,6 @@
-import Container from "@/components/container";
-import PostHeader from "@/components/post-header";
+import Image from "next/image";
+import Container from "components/container";
+import PostHeader from "components/post-header";
 import { getPostBySlug } from "lib/api";
 
 export default function Schedule({
@@ -13,6 +14,17 @@ export default function Schedule({
     <Container>
       <article>
         <PostHeader title={title} subtitle="Blog Article" publish={publish} />
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt="アイキャッチ画像"
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes="(min-width: 1152px) 1152, 100vw"
+            style={{ width: "100%", height: "auto" }}
+            priority
+          />
+        </figure>
       </article>
     </Container>
   );
