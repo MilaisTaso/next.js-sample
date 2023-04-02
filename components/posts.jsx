75b18f9c73a5c455228fab7e3f0c1046307828b6
MilaisTaso <livreblanche819@gmail.com>
2023-04-02 20:31:02 +0900

    edit post components Image element

--- components/posts.jsx
+++ components/posts.jsx
@@ -2,23 +2,27 @@ import styles from "styles/posts.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Posts({posts}) {
+export default function Posts({ posts }) {
   console.log(posts.eyecatch);
   return (
     <div className={styles.gridContainer}>
-    {posts.map(({title, slug, eyecatch}) => (
-      <article className={styles.post} key={slug}>
-        <Link href={`/blog/${slug}`}>
-          <Image src={eyecatch.url}
-           alt=""
-           width={eyecatch.width}
-           height={eyecatch.height}
-           blurDataURL={eyecatch.blurDataURL}
-           />
-          <h2>{title}</h2>
-        </Link>
-      </article>
-    ))}
+      {posts.map(({ title, slug, eyecatch }) => (
+        <article className={styles.post} key={slug}>
+          <Link href={`/blog/${slug}`}>
+            <figure>
+              <Image
+                src={eyecatch.url}
+                alt=""
+                fill
+                sizes="(min-Width: 1152px) 576px, 50vw)"
+                style={{ objectFit: "cover" }}
+                blurDataURL={eyecatch.blurDataURL}
+              />
+            </figure>
+            <h2>{title}</h2>
+          </Link>
+        </article>
+      ))}
     </div>
-    )
+  );
 }
